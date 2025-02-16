bfe3e3b6eaac9af05265bbd29070d637b0a65bee
singhanat <ainghast@gmail.com>
2025-02-16 22:27:34 +0700

    config noteapp.py

--- SimpleWebwithFlask/noteapp.py
+++ SimpleWebwithFlask/noteapp.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///soundshare.db'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
