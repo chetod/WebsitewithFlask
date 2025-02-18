7661ab3940d49896c8c407d8200f79c9826e1374
singhanat <ainghast@gmail.com>
2025-02-18 12:15:50 +0700

    improve login in noteapp.py

--- SimpleWebwithFlask/noteapp.py
+++ SimpleWebwithFlask/noteapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash,request,session
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
@@ -38,13 +38,24 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-@app.route('/login')
+@app.route('/login',methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        print(username, password)
+        user = User.query.filter_by(username=username).first()
+        
+        if user and check_password_hash(user.password, password):
+            user.failed_attempts = 0
+            db.session.commit()
+            
+            session.permanent = True
+            session['user_id'] = user.id
+            flash('เข้าสู่ระบบสำเร็จ', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Username หรือ Password ไม่ถูกต้อง', 'danger')
         return redirect(url_for('index'))
 
     return render_template('login.html', form=form)
