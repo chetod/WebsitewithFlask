e11d37ecca3f0c4981b4a1d996049b64c12f7e20
singhanat <ainghast@gmail.com>
2025-02-18 11:59:15 +0700

    improve login can  connect form to main

--- SimpleWebwithFlask/noteapp.py
+++ SimpleWebwithFlask/noteapp.py
@@ -2,21 +2,23 @@ from flask import Flask, render_template, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
-from forms import RegisterForm
+from forms import RegisterForm,LoginForm
 from models import db, User
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///soundshare.db'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
+db.init_app(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-
     if form.validate_on_submit():
         username = form.username.data
         email = form.email.data
@@ -34,6 +36,20 @@ def register():
             
         flash('ลงทะเบียนสำเร็จ กรุณาเข้าสู่ระบบ', 'success')
         return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        print(username, password)
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
