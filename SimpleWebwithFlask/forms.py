352cb89c1938d79ae20f0aa4e058b3048a8bdb85
singhanat <ainghast@gmail.com>
2025-02-19 11:49:46 +0700

    createsoundpost form

--- SimpleWebwithFlask/forms.py
+++ SimpleWebwithFlask/forms.py
@@ -5,11 +5,20 @@ from flask_wtf.file import FileRequired, FileAllowed
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=80)])
-    email = StringField('Email', validators=[DataRequired(), Length(max=120)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6), Email(message="Invalid email format")])
+    email = StringField('Email', validators=[DataRequired(), Length(max=120), Email(message="Invalid email format")])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('ลงทะเบียน')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('เข้าสู่ระบบ')
+
+class SoundPostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(max=100)])
+    description = TextAreaField('Description')
+    category = SelectField('Category', coerce=int, validators=[DataRequired()])
+    sound_file = FileField('Sound File', validators=[
+        FileRequired(),
+        FileAllowed(['mp3', 'wav'], 'Audio files only!')
+    ])
