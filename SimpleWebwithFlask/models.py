3f62eecca5a2f612fe2e7e8159032042cd309267
singhanat <ainghast@gmail.com>
2025-02-19 14:27:27 +0700

    add new table as category in db(models.py)

--- SimpleWebwithFlask/models.py
+++ SimpleWebwithFlask/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 db = SQLAlchemy()
 
 # กำหนดโมเดล User
@@ -9,4 +10,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)  # อีเมล (ต้องไม่ซ้ำและไม่เป็นค่าว่าง)
     password = db.Column(db.String(256), nullable=False)  # รหัสผ่าน (ต้องไม่เป็นค่าว่าง)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)  # วันที่สร้าง (ค่าเริ่มต้นคือวันที่ปัจจุบัน)
-    
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    posts = db.relationship('SoundPost', backref='category', lazy=True)
+
+class SoundPost(db.Model):
+    pass
