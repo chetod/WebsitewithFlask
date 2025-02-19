9bc157f13df524efc1faef4208e61480cfbb69a4
singhanat <ainghast@gmail.com>
2025-02-19 14:30:27 +0700

    add new table as Comment  in models.py

--- SimpleWebwithFlask/models.py
+++ SimpleWebwithFlask/models.py
@@ -25,3 +25,10 @@ class SoundPost(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('sound_post.id'), nullable=False)
